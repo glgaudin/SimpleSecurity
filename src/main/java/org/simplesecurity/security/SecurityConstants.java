8aec443e88eb53c20a23e97c8e324a521e48507e
glgau <glgau@DESKTOP-GODMBGN>
2018-08-03 16:42:04 -0400

    added work around for properties injected into static variables

--- src/main/java/org/simplesecurity/security/SecurityConstants.java
+++ src/main/java/org/simplesecurity/security/SecurityConstants.java
@@ -1,40 +1,69 @@
 package org.simplesecurity.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 /**
  * 
  * Security related constants
  *
  */
+@Component
 public class SecurityConstants {
 	
-    @Value("${security.securitytoken}")
 	public static String HEADER_SECURITY_TOKEN;
+    @Value("${security.securitytoken}")
+    public void setSecurityToken(String value) {
+    	HEADER_SECURITY_TOKEN = value;
+    }
 	
-    @Value("${security.delimiter}")
 	public static String DELIMITER;
-	
-    @Value("${security.datetimeformat}")
+    @Value("${security.delimiter}")
+    public void setDelimiter(String value) {
+    	DELIMITER = value;
+    }
+    
 	public static String DATETIME_FORMAT;
+    @Value("${security.datetimeformat}")
+    public void setDateFormat(String value) {
+    	DATETIME_FORMAT = value;
+    }
 	
-    @Value("${security.keystrategy}")
 	public static Boolean KEY_STRATEGY;
+    @Value("${security.keystrategy}")
+    public void setKeyStrategy(Boolean value) {
+    	KEY_STRATEGY = value;
+    }
 	
-    @Value("${security.msg.invalidlogin}")
 	public static String INVALID_LOGIN;
+    @Value("${security.msg.invalidlogin}")
+    public void setInvalidLogin(String value) {
+    	INVALID_LOGIN = value;
+    }
 	
-    @Value("${security.msg.invaliduser}")
 	public static String INVALID_USER;
+    @Value("${security.msg.invaliduser}")
+    public void setInvaliduser(String value) {
+    	INVALID_USER = value;
+    }
 
-    @Value("${security.msg.nopermission}")
 	public static String NO_PERMISSION;
+    @Value("${security.msg.nopermission}")
+    public void setNoPermission(String value) {
+    	NO_PERMISSION = value;
+    }
 
-    @Value("${security.msg.invalidpermission}")
 	public static String INVALID_PERMISSION;
+    @Value("${security.msg.invalidpermission}")
+    public void setInvalidPermission(String value) {
+    	INVALID_PERMISSION = value;
+    }
 
-    @Value("${security.salt}")
 	public static String SALT;
+    @Value("${security.salt}")
+    public void setSalt(String value) {
+    	SALT = value;
+    }
 
     public static KeyAlgorithms KEY_ALGORITHM = KeyAlgorithms.AES;
 	
